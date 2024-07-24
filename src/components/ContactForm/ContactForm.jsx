11c9b03cb60a9d5a230e7d3e04d27b9ae0a73644
Ilya Svintsov <xeri237@gmail.com>
2024-07-24 12:52:43 +0300

    new commit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid'
 import PropTypes from "prop-types";
 import * as Yup from 'yup';
 
+
 const SubmitButton = styled.button`
     width:100px;
 `
@@ -17,13 +18,16 @@ const ContactsForm = styled(Form)`
     margin-bottom:20px;
 `
 
+const Input = styled(Field)`
+`
+
 const Error = styled(ErrorMessage)`
     color:red;
 `
 
 const validationSchema = Yup.object({
-    name: Yup.string().required('Please enter your name'),
-    number: Yup.string().min(10, 'Number too short!').max(10, 'Number too long!').required('Please enter a number!'),
+    name: Yup.string().min(3, 'Name too short!').max(50, 'Name too long!').required('Please enter your name'),
+    number: Yup.string().min(3, 'Number too short!').max(50, 'Number too long!').required('Please enter a number!'),
 })
 const ContactForm = ({addNumber}) => {
     const id = {
@@ -48,13 +52,13 @@ const ContactForm = ({addNumber}) => {
         <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
             <ContactsForm>
                 <label htmlFor={id.name}>Name</label>
-                <Field type='text' name='name' id={id.name}/>
+                <Input type='text' name='name' id={id.name}/>
                 <Error name='name' component='span' />
                 <label htmlFor={id.number}>Number</label>
-                <Field type='text' name='number' id={id.number}></Field>
+                <Input type='text' name='number' id={id.number}></Input>
                 <Error name='number' component='span' />
 
-                <Error type='submit'>Submit</Error>
+                <SubmitButton type='submit'>Submit</SubmitButton>
             </ContactsForm>
         </Formik>
     )
